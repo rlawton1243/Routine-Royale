af4c64e6b83a6ab8abe07fb00e16f73f59d85b46
Ben Clermont <31889040+benclermontt@users.noreply.github.com>
2021-04-03 20:20:58 -0400

    Flipped relationship between Event and Task
    This meant I had to change 2 vals in the Event and Task serializers...
    
    I can't verify this change as they still have that -detail hyperlink issue from before.
    
    However it doesn't crash on boot so it's reading the vals correctly.

--- royale/views.py
+++ royale/views.py
@@ -1,17 +1,21 @@
 """
 Django Views
+https://www.django-rest-framework.org/tutorial/3-class-based-views/
+https://stackoverflow.com/questions/21508982/add-custom-route-to-viewsets-modelviewset
 """
-from django.http import Http404
-from rest_framework.decorators import api_view
+from django.http import Http404, HttpResponse
+from rest_framework.decorators import api_view, action
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-
+from rest_framework.renderers import JSONRenderer
 from royale.models import Client, EventParticipation, TaskSchedule, TaskStep, Task, Event, UserAction
 
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, renderers
 from rest_framework import permissions
 from rest_framework import generics
 from rest_framework.views import APIView
+
 
 from django.contrib.auth.models import User
 
@@ -30,15 +34,25 @@ class ClientViewSet(viewsets.ModelViewSet):
 class EventViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Events to be viewed or edited.
+    https://www.django-rest-framework.org/api-guide/viewsets/
+    https://www.django-rest-framework.org/tutorial/6-viewsets-and-routers/
     """
-    queryset = Event.objects.all().order_by('-id')
+
+    queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(methods=['GET'], detail=False, renderer_classes=[renderers.StaticHTMLRenderer])
+    def public(self, request):
+        qs = Event.objects.filter(is_public=True).order_by('-id')
+        serializer = EventSerializer(instance=qs, many=True)
+        return Response(JSONRenderer().render(serializer.data), content_type='json')
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows tasks to be viewed or edited.
+    https://stackoverflow.com/questions/53760772/how-to-create-a-custom-action-endpoint-that-corresponds-to-put-and-executes-a-fu
     """
     queryset = Task.objects.all().order_by('-id')
     serializer_class = TaskSerializer
@@ -60,6 +74,17 @@ class ClientCreateAPIView(generics.CreateAPIView):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
     permission_classes = (AllowAny,)
+
+
+class EventParticipationViewSet(viewsets.ModelViewSet):
+    queryset = EventParticipation.objects.all()
+    #serializer_class = EventParticipationSerializer
+    permission_classes = (AllowAny, )
+
+    @action(methods=['post'], detail=True)
+    def join_event(self, request, pk=None):
+        pass
+
 
 
 @api_view(['POST', 'GET'])
