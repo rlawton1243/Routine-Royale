c4921e1758a503fe7488c9898f7804ce433c9ac0
Jordan Blackadar <jordan.blackadar@outlook.com>
2021-04-05 15:54:51 -0400

    Fix CSRF interaction from GUI Fix join_event issue Added test_login endpoint

--- royale/urls.py
+++ royale/urls.py
@@ -15,4 +15,5 @@ router.register(r'participations', views.EventParticipationViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('test_login/', views.test_login)
 ]
