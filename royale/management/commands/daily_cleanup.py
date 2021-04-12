c6ca00c8697f85184c4dc9ceb30796a7876028b4
Ben Clermont <31889040+benclermontt@users.noreply.github.com>
2021-04-12 19:52:29 -0400

    Added daily_cleanup management task
    Redid the event key storage so it doesnt' generate a new one everytime str gets called
    
    Redid a few relations

---
+++ royale/management/commands/daily_cleanup.py
@@ -0,0 +1,121 @@
+"""
+Daily Cleanup will be a python script that does the following functions
+
+For Every Event in System:
+
+Check if event is complete
+
+- if event is complete divy up points to all event participators
+    Points = num total_completed + (.25 * streak * total_completed)
+    - Don't delete anything right now we'll do that later
+
+
+- otherwise...
+
+1. Find all Tasks (and Steps) which were due on the current date
+Tasks fetched, skipping steps until later
+
+2. For Every EventParticipation related to the current Event:
+    a. If the Task(s) (and steps) were completed increment total_completed
+        Done and Tested!
+
+    b. If ANY Tasks (or steps) were missed set streak to 0
+        I. Else Increment streak by 1
+         Done and Tested!
+
+    c. Clear completed tasks and completed steps
+         Done and Tested!
+3. Set Task (and Step) Due Date/Times to next in Schedule if they were due Today and are repeating
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+from royale.models import Event, Task, EventParticipation
+from datetime import *
+
+
+class Command(BaseCommand):
+    help = 'Performs the Daily Cleanup of Events and Assigns points to users'
+
+    def handle(self, *args, **options):
+        events = Event.objects.all()
+
+        for event in events:
+            print(event)
+            # All Tasks
+            today = datetime.now().replace(hour=23, minute=59)
+            curr_weekday = datetime.today().weekday()
+            tasks_due_today = event.task_set.filter(due_time__lte=today)
+
+            # Gets QuerySet of all participators in current event
+            participators = event.eventparticipation_set.all()
+
+            for participant in participators:
+                """
+                For each participator in the event update task related stats
+                """
+                completed_today = participant.completed_tasks.all()
+                print(participant)
+                print(completed_today)
+
+                participant.total_completed += len(list(completed_today))
+
+                if set(completed_today) == set(tasks_due_today):
+                    participant.streak += 1
+                else:
+                    participant.streak = 0
+
+                participant.completed_tasks.clear()
+                participant.save()
+
+            print('Tasks in Event')
+            for task in tasks_due_today:
+                print(task)
+
+                if task.schedule is None:
+                    """
+                    If Task does not have a schedule task will be deleted
+                    """
+                    # TODO: Delete Task if it does not have schedule
+                    continue
+
+                else:
+                    """
+                    If Task does have a schedule the tasks due date will be updated to the new due date
+                    """
+
+                    schedule = [1 if task.schedule.monday else 0,
+                                1 if task.schedule.tuesday else 0,
+                                1 if task.schedule.wednesday else 0,
+                                1 if task.schedule.thursday else 0,
+                                1 if task.schedule.friday else 0,
+                                1 if task.schedule.saturday else 0,
+                                1 if task.schedule.sunday else 0]
+                    print(schedule)
+
+                    # Monday updating through sunday
+                    if curr_weekday == 0 and 1 in schedule[1:]:
+                        task.due_time = task.due_time + timedelta(days=schedule[1:].index(1)-curr_weekday+1)
+                    # Tuesday updating through sunday
+                    elif curr_weekday == 1 and 1 in schedule[2:]:
+                        task.due_time = task.due_time + timedelta(days=schedule[2:].index(1)-curr_weekday+1)
+                    # Wednesday
+                    elif curr_weekday == 2 and 1 in schedule[3:]:
+                        task.due_time = task.due_time + timedelta(days=schedule[3:].index(1)-curr_weekday+1)
+                    # Thursday
+                    elif curr_weekday == 3 and 1 in schedule[4:]:
+                        task.due_time = task.due_time + timedelta(days=schedule[4:].index(1)-curr_weekday+1)
+                    # Friday
+                    elif curr_weekday == 4 and 1 in schedule[5:]:
+                        task.due_time = task.due_time + timedelta(days=schedule[5:].index(1)-curr_weekday+1)
+                    # Saturday
+                    elif curr_weekday == 5 and 1 in schedule[6]:
+                        task.due_time = task.due_time + timedelta(days=schedule[6:].index(1)-curr_weekday+1)
+                    # Sunday and there is a 1 in the schedule and repeating is enabled it goes to next available
+                    if task.repeating and 1 in schedule:
+                        task.due_time = task.due_time + timedelta(days=schedule.index(1)+1)
+                    elif not task.repeating:
+                        pass
+                        # TODO: Delete the Task.
+
+                task.save()
+            print('\n')
